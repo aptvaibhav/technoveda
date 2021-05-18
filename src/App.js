a973cbe9c84e2d3408e4b4ce6da465c9bd31a7b7
vaibhav tyagi <vaibhav31122@gmail.com>
2021-05-19 00:00:59 +0530

    commit message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Diagonse from '././components/Diagonse/Diagnose';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +17,16 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <div class="card" style="width: 18rem;">
+  {/* <img class="card-img-top" src="..." alt="Card image cap"> </img> */}
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+    
+    </div>
       </header>
     </div>
   );
---
+++ src/components/Pages/Landing/Landing.js
@@ -0,0 +1,99 @@
+import React from "react";
+import curved from "../assets/curved.png";
+import vector from "../assets/vector.png";
+export default function Landing() {
+    return (
+        <>
+        <div className="landing">
+        <img className="curved" src={curved} alt="curved" />
+
+        <div className="center">
+          <h4 className="text-center"> Welcome to Technoveda</h4>
+          <h1 className="text-center"> Get yourself Diagnosed for free with us.</h1>
+          <button
+            type="button"
+            className="btn btn-light need-blood-btn"
+            
+          >
+            Diagnose your disease?
+          </button>
+        </div>
+      </div>
+      <div className="features">
+        <h2> Features of this App</h2>
+        <div className="feature-body">
+          <div className="image">
+            <img src={vector} alt="vector" />
+          </div>
+          <div className="points">
+            <div className="bullets">
+              <h4>
+                <i className="fa fa-location-arrow" aria-hidden="true"></i>
+                Disease Diagnosing
+              </h4>
+              <p>
+                {" "}
+                Get all your diseases diagnosed for free.
+                Now no need to wait get yourself diagnosed! 
+              </p>
+            </div>
+            <div className="bullets">
+              <h4>
+                {" "}
+                <i className="fa fa-clock-o" aria-hidden="true"></i>Real Time
+                Connect
+              </h4>
+              <p>
+                No delays in getting consultation. Connect with doctors
+                <br /> and get disease diagnosed in real time
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="tips" id="tips">
+        <h2> Tips</h2>
+        <p>
+          {" "}
+          Here are some tips to put your mind on if you want to live
+          a healthy and fit lifestyle.
+        </p>
+        <div className="container">
+          <div className="tips-info">
+            <h4>Diagnose your Diseases</h4>
+            <hr className="horizontal" />
+            <ul>
+              <li>
+                Regular diagnosing of diseases is neccessary
+              </li>
+              <li>Have a proper sleep of atleast 8 hours</li>
+              <li>Include more liquids in your diet</li>
+            </ul>
+          </div>
+          <div className="tips-info">
+            <h4> Doctors Consultation</h4>
+            <hr className="horizontal" />
+            <ul>
+              <li>
+                If you diagnosed and show positive consult a doctor immediately
+              </li>
+            
+              <li> Avoid eating fast food</li>
+            </ul>
+          </div>
+          <div className="tips-info">
+            <h4>Some other tips</h4>
+            <hr className="horizontal" />
+            <ul>
+              <li>
+                Dont hasitate in asking questions
+              </li>
+              <li>Asks all your doubts and quaries with the doctors</li>
+              <li>Follow a proper routine and lifestyle</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      </>
+        );
+    }
